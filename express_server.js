b5b623e5d6007900a2b6730b8545eed2c59b0810
Henry Boyd <AmorphousCube@gmail.com>
2020-01-20 21:43:51 +0000

    completed part 1

--- express_server.js
+++ express_server.js
@@ -2,7 +2,20 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+function generateRandomString() {
+  //returns a random-ish 6 character string
+  let result = '';
+  let chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  while (result.length < 6) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length))
+  }
+  return result;
+}
+
 app.set("view engine", "ejs");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -13,6 +26,15 @@ const urlDatabase = {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 })
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]/* What goes here? */ };
