5ee7c5a11fd593016b34b663fff4b2a7c3cd05bf
Henry Boyd <AmorphousCube@gmail.com>
2020-01-20 20:04:34 +0000

    first ejs template

--- express_server.js
+++ express_server.js
@@ -6,11 +6,17 @@ app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
+  //"test" : "http://www.example.com"
 };
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+})
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/hello", (req, res) => {
